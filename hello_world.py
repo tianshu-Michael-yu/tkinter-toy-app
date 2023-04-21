02b5c8aacb457b694384cc1f6d8b4c73b0e31d99
ty113 <ty113@duke.edu>
2023-04-21 12:43:21 -0400

    I placed a label on the root window

--- hello_world.py
+++ hello_world.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Hello World")
+
+# place a label on the root window
+message = tk.Label(root, text="Hello, World!")
+message.pack()
+
+# keep the window open until the user closes it
 root.mainloop()
