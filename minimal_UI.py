0c34bc1e41558c3b59f6968ac3a492126c7dcfa2
ty113 <ty113@duke.edu>
2023-05-02 18:09:42 -0400

    I wrote two UI.

--- minimal_UI.py
+++ minimal_UI.py
@@ -10,28 +10,19 @@ class App:
         self.root = tk.Tk()
         self.root.title("Matplotlib in Tkinter App")
         self.root.geometry("800x600")
-        self.tab_control = ttk.Notebook(self.root)
 
-    def plot(self, master, x_data, y_data):
+    def plot(self, x_data, y_data):
         fig = Figure(figsize=(6, 6))
         ax = fig.add_subplot(111)
         ax.plot(x_data, y_data)
-        figure_canvas = FigureCanvasTkAgg(fig, master=master)
+        figure_canvas = FigureCanvasTkAgg(fig, master=self.root)
         figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def show(self):
         self.root.mainloop()
 
-    def create_tab(self, tab_name = "tab"):
-        tab = ttk.Frame(self.tab_control)
-        self.tab_control.add(tab, text=tab_name)
-        self.tab_control.pack(expand=1, fill='both')
-        return tab
 
 if __name__ == "__main__":
     app = App()
-    tab2 = app.create_tab("tab2")
-    app.plot(tab2, [1, 2, 3, 4, 5], [3, 5, 2, 6, 2]) 
-    tab1 = app.create_tab("tab1") 
-    app.plot(tab1, [1, 2, 3, 4, 5], [3, 5, 2, 6, 2])
+    app.plot([1, 2, 3, 4, 5], [3, 5, 2, 6, 2])
     app.show()
